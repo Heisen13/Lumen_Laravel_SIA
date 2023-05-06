87e1d05e47907c52e5aa0f6a063b82f3c631d30b
Heisen13 <sarip.mohammad13@gmail.com>
2023-05-06 19:41:10 +0800

    Act 2

---
+++ app/Models/User.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class User extends Model{
+
+protected $table = 'students';
+
+protected $fillable = [
+'Student_ID', 'Student_FName', 'Student_LName'
+];
+}
