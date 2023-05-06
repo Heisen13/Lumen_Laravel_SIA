48be2fc47d9d1922593a53eaedf77e7f501034af
Heisen13 <sarip.mohammad13@gmail.com>
2023-05-06 21:09:44 +0800

    Act 3-4

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,4 +11,8 @@ protected $table = 'students';
 protected $fillable = [
 'Student_ID', 'Student_FName', 'Student_LName'
 ];
+
+public $timestamps = false;
+protected $primaryKey = 'Student_ID';
+
 }
